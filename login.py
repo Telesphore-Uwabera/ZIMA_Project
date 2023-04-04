0690fb6cfea3e68a6d7f3c64be7306592c29a922
Isaiah-Essien <i.essien@alustudent.com>
2023-04-05 01:00:55 +0200

    the complete code for ZIMA and the SQL database from import

---
+++ login.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from datetime import datetime
+
+db=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Klienty@7322',
+    database='login_zima'
+)
+my_cursor=db.cursor()
+
+#my_cursor.execute("CREATE DATABASE login")
+
+#my_cursor.execute("CREATE TABLE user_login (NAME VARCHAR(500) NOT NULL, PASSWORD VARCHAR(500) NOT NULL, EMAIL VARCHAR(500) NOT NULL, AGE smallint NOT NULL, DATE_CREATED datetime NOT NULL)")
+
+#my_cursor.execute("ALTER TABLE user_login ADD COLUMN DATE_CREATED datetime NOT NULL")
+
+'''my_cursor.execute("SELECT * FROM user_login")
+for x in my_cursor:
+    print(x)
+    
+    
+#my_cursor.execute("INSERT INTO user_login (NAME, PASSWORD, EMAIL, AGE) VALUES (%s,%s,%s,%s)", ("your name", "@123Ab", "youremail@gmail.com", 45))
+#db.commit()'''
+
+'''name= input("Enter your name: ")
+passwd= input("Enter a strong password: ")
+email= input("enter your email address: ")
+age= input('Enter your age: ')
+print('Your information succesfully received')
+my_cursor.execute("INSERT INTO user_login (NAME, PASSWORD, EMAIL, AGE, DATE_CREATED) VALUES (%s,%s,%s,%s,%s)", (name, passwd, email, age, datetime.now()))
+db.commit()'''
+#these lines of code are commented in order to avoid execution that may disrupt the menu app
+my_cursor.execute("SELECT * FROM user_login")
--- men-app.py
+++ men-app.py
@@ -1,91 +1,108 @@
-#this code is an AI driven appliction based on symptoms
-#Malaria=['headache', 'fever', 'flu']
-#Covid_19=['sore-throath', 'pink eyes', 'muscle and joint pain']
+# !/usr/bin/python3
+import webbrowser
+from login import my_cursor
 
-'''while True:
-    Q_1=input('Are you sick??(y/n):')
-    if Q_1.casefold()=='n':
-        print('Thank you for using ZIMA')
-        quit()
-    else:
-        Q_2=input('Do you feel feverish, cold, Headache, flu? (y/n):')
-        if Q_2.casefold()== 'y':
-            print('You show symptoms of Malaria')
-            Q_3=input('Do you want to see a doctor??(y/n):')
-            if Q_3.casefold()=='y':
-                print('Link to a doctor')
-                quit()
-            else:
-                print('Thank you for using Zima')
-                quit()
+database=my_cursor.fetchall()
+def check_login(name, passwd):
+    new= database
+    for user in new:
+        if name in user and passwd in user:
+            # Return the password if the name is found
+            return True
+
+print('WELCOME TO ZIMA')
+
+
+while True:
+    try:
+        login = input('Do you have an account? (y/n): ').casefold()
+        if login not in ['y', 'n']:
+            raise ValueError
         else:
             break
-while True:
-    Q_4=input('do you feel muscle and joint pains, pink eyes, sore-throath??(y/n):')
-    if Q_4=='y':
-        print('You have symptoms of Covid_19')
-        Q_5=input('do you want to see a doctor??(y/n):')
-        if Q_5.casefold()=='y':
-            print('link to a doctor')
-            quit()
-        else:
-            print('thank you for using ZIMA')
-            quit()
-    else:
-        print('Thank you for using ZIMA')
-        break
-quit()'''
+    except ValueError:
+        print("Invalid input. Please input 'y' or 'n'.")
+if login == 'y':
+    while True:
+        try:
+            name = input("Please enter your name: ")
+            passwd = input("Enter password: ")
+            check_login(name, passwd)
+            if check_login(name, passwd):
+                print("Login successful")
+                break
+            else:
+                print("Wrong details. Do you want to try again or visit the login page to register?")
+                choice = input("Please enter 'try again' or 'visit': ").casefold()
+                if choice == 'try again':
+                    continue
+                elif choice == 'visit':
+                    print("Thank you")
+                    quit()
+                else:
+                    print("Invalid input, please try again.")
+        except:
+            print("Wrong input, please try again")
+elif login == 'n':
+    print("Please visit the sign in page to register")
+    quit()
+else:
+    raise ValueError("Invalid input, please try again.")
 
 
-import webbrowser
 
-# Define the symptoms and illnesses.
-symptoms = {
-    'fever, headache and chills': 'malaria',
-    'diarrhea, vomiting, leg cramps, thrist,  irritability': 'cholera',
-    'cough, sneezing, runny nose, sore throat, high temperature': 'covid-19',
+# Define the symptoms and illnesses as a dictionary.
+SYMPTOMS = {
+    'fever, headache, and chills': {
+        'illness': 'malaria',
+        'description': 'Malaria is a disease caused by a parasite. It is spread through the bite of an infected mosquito. The symptoms include fever, headache, and chills. Treatment usually involves medication.',
+        'link': 'https://www.who.int/news-room/fact-sheets/detail/malaria'
+    },
+    'diarrhea, vomiting, leg cramps, thirst, and irritability': {
+        'illness': 'cholera',
+        'description': 'Cholera is a bacterial infection that can cause severe diarrhea and dehydration. It is spread through contaminated water or food. Treatment involves rehydration therapy and antibiotics.',
+        'link': 'https://www.who.int/news-room/fact-sheets/detail/cholera'
+    },
+    'cough, sneezing, runny nose, sore throat, and high temperature': {
+        'illness': 'covid-19',
+        'description': 'COVID-19 is a respiratory illness caused by the SARS-CoV-2 virus. It is spread through respiratory droplets when an infected person talks, coughs, or sneezes. The symptoms include cough, fever, and difficulty breathing. Treatment involves supportive care and in some cases, antiviral medication.',
+        'link': 'https://www.who.int/health-topics/coronavirus#tab=tab_1'
+    },
     # Add more symptoms and illnesses here
 }
 
-# Ask the user to choose a symptom
+# Ask the user to choose a symptom.
 while True:
-    print('Please choose a symptom:')
-    for i, symptom in enumerate(symptoms.keys()):
+    print('Please choose a list of symptoms:')
+    for i, symptom in enumerate(SYMPTOMS.keys()):
         print(f'{i+1}. {symptom}')
     try:
         choice = int(input('Enter a number: '))
-        symptom = list(symptoms.keys())[choice-1]
+        symptom = list(SYMPTOMS.keys())[choice-1]
         break
     except (ValueError, IndexError):
         print('Invalid input, please try again.')
 
-# Prompt the user with the name of the illness
-illness = symptoms[symptom]
-print(f'You may have {illness}.')
 
-# Ask the user if they want to see a doctor
+# Prompt the user with the name of the illness and its description.
+illness = SYMPTOMS[symptom]['illness']
+description = SYMPTOMS[symptom]['description']
+print(f'You may have {illness}.')
+print(description)
+
+# Ask the user if they want to learn more or see a doctor.
 while True:
-    choice = input('Do you want to see a doctor? (yes/no) ').casefold()
-    if choice == 'yes':
+    choice = input('Do you want to learn more about your illness or see a doctor? (learn/doctor/none) ').casefold()
+    if choice == 'learn':
+        webbrowser.open(SYMPTOMS[symptom]['link'])
+        break
+    elif choice == 'doctor':
         webbrowser.open('https://www.nyarugengehospital.gov.rw/services/doctors')
         break
-    elif choice == 'no':
-        while True:
-            try:
-                learn_more = input('Do you want to learn more about your illness? (yes/no): ').casefold()
-                if learn_more == 'yes':
-                    webbrowser.open('https://www.who.int/news-room/questions-and-answers/item/malaria?gclid=Cj0KCQjwiZqhBhCJARIsACHHEH-v6nXsP2nyyCfMyx17mZ4hMxV1Vcw-QqL1uDO6V9MlFXCXmJCBV4EaAuUNEALw_wcB')
-                    break
-                elif learn_more == 'no':
-                    break
-                else:
-                    print('Invalid input, please try again.')
-            except ValueError:
-                print('Invalid input, please try again.')
-        break  # Exit the loop and end the program after the user chooses to learn more
+    elif choice == 'none':
+        break
     else:
         print('Invalid input, please try again.')
 
-# Close the program with a message
-print('Thank you for using ZIMA')
-
+# Close the program with a message.
+print('Thank you for using ZIMA.')
